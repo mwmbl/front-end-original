d623edf94113c49e0fe013cca9169ace8152f907
ColinEspinas <ColinEspinas@users.noreply.github.com>
2022-08-14 16:27:58 +0000

    deploy: fda48026b9f7c45913fa679c838be9cbccb78b55

---
+++ assets/app.a1279ed1.js
@@ -0,0 +1,13 @@
+import{d as s}from"./define.b7078a0a.js";import"./config.d02c906f.js";const e=()=>`
+  <header class="search-menu">
+    <div class="branding">
+      <img class="brand-icon" src="../assets/images/logo.svg" width="40" height="40" alt="mwmbl logo">
+      <span class="brand-title">MWMBL</span>
+    </div>
+    <mwmbl-search-bar></mwmbl-search-bar>
+  </header>
+    <main>
+      <mwmbl-results></mwmbl-results>
+    </main>
+  <footer is="mwmbl-footer"></footer>
+`,r=s("app",class extends HTMLElement{constructor(){super(),this.__setup()}__setup(){this.innerHTML=e()}});export{r as default};
