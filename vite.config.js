6c6bdaeeb9f826074ea6efb063ce91395d6a8aa6
EchedeyLR <elr@disroot.org>
2022-12-05 23:57:22 +0000

    Added support for older browsers

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import legacy from '@vitejs/plugin-legacy'
+
 export default {
   root: './src',
   publicDir: '../assets',
   build: {
-    outDir: '../dist',
-  }
+    outDir: '../dist'
+  },
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ]
 }
