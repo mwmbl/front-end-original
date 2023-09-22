5ed4ed68cfa81f0e75d2ccad7d8691fb38bab918
groundcat <11240246+groundcat@users.noreply.github.com>
2023-09-22 06:57:45 +0000

    Simplify Landing Page Text

--- src/components/organisms/home.js
+++ src/components/organisms/home.js
@@ -2,11 +2,8 @@ import define from '../../utils/define.js';
 
 const template = () => /*html*/`
   <h1>
-    Welcome to mwmbl, the free, open-source and non-profit search engine.
+    A free, open-source, and non-profit search engine.
   </h1>
-  <p>
-    You can start searching by using the search bar above!
-  </p>
 `;
 
 export default define('home', class extends HTMLLIElement {
