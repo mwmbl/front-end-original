d581040a7ee2d48826a64ab0c348b0d85493d098
Daoud Clarke <daoud.clarke@gmail.com>
2023-10-07 15:21:10 +0100

    White space

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default {
   publicDir: '../assets',
   build: {
     outDir: '../dist',
-      rollupOptions: {
+    rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
         stats: resolve(__dirname, 'src/stats/index.html'),
